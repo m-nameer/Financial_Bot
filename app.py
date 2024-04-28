c3e0684fdd06582b910c68abc5cf7ed18fe24ed9
m-nameer <nameershafi@gmail.com>
2024-04-28 19:12:56 +0500

    Use existing vector store from pinecone

--- app.py
+++ app.py
@@ -47,33 +47,47 @@ def get_text_chunks(text):
 
 
 def get_vectorstore(docs):
-    embeddings = OpenAIEmbeddings()
+    # embeddings = OpenAIEmbeddings()
 
     
-    vectorstore_from_docs = PineconeVectorStore.from_documents(
-        docs,
-        index_name="fibot",
-        embedding=embeddings
-    )
+    # vectorstore_from_docs = PineconeVectorStore.from_documents(
+    #     docs,
+    #     index_name="finbot",
+    #     embedding=embeddings
+    # )
+
+    vectorstore_from_docs = []
     
     return vectorstore_from_docs
 
 
-def get_conversation_chain(vectorstore):
+def get_conversation_chain():
+    embeddings = OpenAIEmbeddings()
+
+    
+
+
     llm = ChatOpenAI()
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
+    # print("Type of retriever:", type(vectorstore.as_retriever()))
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
+    
+    docsearch = PineconeVectorStore.from_existing_index(index_name="fibot", embedding=embeddings)
+    
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        retriever=vectorstore.as_retriever(),
+        retriever=docsearch.as_retriever(),
         memory=memory
     )
+   
     return conversation_chain
 
 
 def handle_userinput(user_question):
+    st.session_state.conversation = get_conversation_chain()
+    # print("user_question", user_question)
+    # print("conversation", st.session_state.conversation)
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
 
@@ -109,36 +123,36 @@ def main():
     if user_question:
         handle_userinput(user_question)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        pdf_docs = st.file_uploader(
-            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                raw_text = get_pdf_text(pdf_docs)
+    # with st.sidebar:
+    #     st.subheader("Your documents")
+    #     pdf_docs = st.file_uploader(
+    #         "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    #     if st.button("Process"):
+    #         with st.spinner("Processing"):
+    #             # get pdf text
+    #             raw_text = get_pdf_text(pdf_docs)
 
-                # get the text chunks
-                text_chunks = get_text_chunks(raw_text)
+    #             # get the text chunks
+    #             text_chunks = get_text_chunks(raw_text)
 
-                # create vector store
-                # vectorstore = get_vectorstore(text_chunks)
-                vectorstore = get_vectorstore(text_chunks)
+    #             # create vector store
+    #             # vectorstore = get_vectorstore(text_chunks)
+    #             vectorstore = get_vectorstore(text_chunks)
 
-                # pc = Pinecone(api_key="147350ef-5846-457f-85e7-55f6bf459f85")
-                # index = pc.Index("financialbot")
+    #             # pc = Pinecone(api_key="147350ef-5846-457f-85e7-55f6bf459f85")
+    #             # index = pc.Index("financialbot")
 
-                # print(index.describe_index_stats())
+    #             # print(index.describe_index_stats())
 
-                # vectors_with_ids = [(f"id_{i}", vector) for i, vector in enumerate(query_result)]
+    #             # vectors_with_ids = [(f"id_{i}", vector) for i, vector in enumerate(query_result)]
 
-                # index.upsert(vectors=vectors_with_ids)
+    #             # index.upsert(vectors=vectors_with_ids)
 
                 
 
-                # create conversation chain
-                st.session_state.conversation = get_conversation_chain(
-                    vectorstore)
+    #             # create conversation chain
+    #             st.session_state.conversation = get_conversation_chain(
+    #                 vectorstore, text_chunks)
 
 
 if __name__ == '__main__':
